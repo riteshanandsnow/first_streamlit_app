ce2351b4ed9a5ece8362e357bbeeca6a6da7c5ec
riteshanandsnow <121084037+riteshanandsnow@users.noreply.github.com>
2022-12-20 14:33:22 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.title('This is Snow Resturant')
+streamlit.title('This is Mina Gupta Resturant')
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
 streamlit.text('Kale, Spinach & Rocket Smoothie')
