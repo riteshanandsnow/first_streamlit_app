0aeca0668c60570c7450df465576f78da63c1510
riteshanandsnow <121084037+riteshanandsnow@users.noreply.github.com>
2022-12-20 14:27:10 +0530

    Menu update and special menu items

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit
-streamlit.title('My name is Ritesh Anand')
+streamlit.title('This is ZZZ Resturant')
 streamlit.header('Breakfast Menu')
-streamlit.title('Bread')
-streamlit.title('Egg')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
